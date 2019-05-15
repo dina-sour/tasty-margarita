3ff3b86b1e0d52e5df26b4cbee7d9b83039312f4
dina-sour <matveev.dina@gmail.com>
2019-05-15 22:14:32 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
+import Zoom from 'react-reveal/Zoom';
 
 const URL = 'https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json';
 
-class App extends Component {
+class App extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      pokemon: []
-    }  
+      pokemon: [],
+      searchTerm: ''
+    } 
+    this.onChange = this.onChange.bind(this) 
   }
 
   componentDidMount() {
@@ -22,34 +25,48 @@ class App extends Component {
       })
   }
 
+  onChange(e){
+    this.setState({
+      searchTerm: e.target.value 
+    })
+  }
+
   render() {
+    
+    const pokemon = this.state.pokemon.map((item)=>{
+      if(!this.state.searchTerm) return <div>{item}</div> //return the items without filter when searchterm is empty
+      const regEx = new RegExp(this.state.searchTerm, 'g') //create regex based on search term to filter the items 
+      return regEx.test(item) && <div>{item}</div> //if the item passes the regex text, return that item
+    })
+
     return (
       <div className="app">
         <div className = "header">
-          Pokédex
+          POKéDEX
         </div>
-          <div className = "pokeGrid">
-            { this.state.pokemon.map(pokemon => 
-          <div className = "pokeCell zoom">
 
-            <div className = "poke-name">{pokemon.name}</div>
+        <input className = "search-bar" type="text" onChange={this.onChange} placeholder='Search for Pokémon...'></input>
 
-            <div className = "poke-cell-content">
+            <div className = "poke-grid">
+              { this.state.pokemon.map(pokemon => 
+              <div className = "poke-cell zoom" onClick = {this.nonesense}>
 
-              <div className = "poke-photo">
-                <img src = {pokemon.img} ></img>
+                    <div className = "poke-name">{pokemon.name}</div>
+
+                    <div className = "poke-cell-content">
+
+                          <div className = "poke-photo">
+                            <img src = {pokemon.img} ></img>
+                          </div>
+                            <div className = "poke-info">
+                                <div className = "poke-number">{pokemon.num}</div>
+                                <div className = "poke-type">Type:  {pokemon.type[0]} </div>
+                            </div>
+                    </div>
               </div>
-                <div className = "poke-info">
-                    <div className = "poke-type">Type:  {pokemon.type[0]} </div>
-                    <div className = "poke-height">Weight:  {pokemon.weight}</div>
-                    <div className = "poke-weight">Height:  {pokemon.height}</div>
-                </div>
-            </div>
-          </div>
-          
-          )}
+            )}
+              </div>
 
-        </div>
         <div className = "footer">
             <p>Made with love by <a href="https://github.com/dina-sour">Dina Matveev</a></p>
         </div>
