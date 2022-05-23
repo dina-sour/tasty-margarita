79c002ce6362dfb76eb0ed901f0a9af51e3924cc
dina-sour <matveev.dina@gmail.com>
2022-05-24 02:11:31 +0300

    Updates

---
+++ precache-manifest.0fa4396a6f4f6d27eb5d4d6b4fabd40a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "706571aa98d716c0cd50",
+    "url": "/tasty-margarita/static/css/main.013e6e8c.chunk.css"
+  },
+  {
+    "revision": "706571aa98d716c0cd50",
+    "url": "/tasty-margarita/static/js/main.be286d6e.chunk.js"
+  },
+  {
+    "revision": "dd958769039ee2cba7a2",
+    "url": "/tasty-margarita/static/js/runtime~main.6d242f76.js"
+  },
+  {
+    "revision": "0eddd1076e6eacfaa552",
+    "url": "/tasty-margarita/static/js/2.1b6d3ebd.chunk.js"
+  },
+  {
+    "revision": "213b05757861b0a915005be6ddcef0ae",
+    "url": "/tasty-margarita/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/tasty-margarita/precache-manifest.0fa4396a6f4f6d27eb5d4d6b4fabd40a.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/tasty-margarita/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
